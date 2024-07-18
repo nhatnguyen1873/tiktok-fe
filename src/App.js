41891fd57ed8b0bca9b45ab593772421c474d983
Nhat Nguyen Minh <ngminhat187@gmail.com>
2024-07-18 22:05:19 +0700

    feat: add routes and layouts (#6)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Fragment } from "react";
+import { publicRoutes } from "@/routes";
+import { DefaultLayout } from "@/components/Layout";
+
 function App() {
     return (
-        <div className="App">
-            <h1>Tiktok ui</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.length > 0 &&
+                        publicRoutes.map((route, index) => {
+                            let Layout = DefaultLayout;
+                            if (route.layout) {
+                                Layout = route.layout;
+                            } else if (route.layout === null) {
+                                Layout = Fragment;
+                            }
+
+                            return (
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    element={
+                                        <Layout>
+                                            <route.component />
+                                        </Layout>
+                                    }
+                                />
+                            );
+                        })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
