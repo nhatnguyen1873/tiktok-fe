9a040eba0fff2be3a9d3c9333fe64dcce6372c74
Nhat Nguyen Minh <ngminhat187@gmail.com>
2024-07-20 11:29:12 +0700

    feat: build the default layout (#8)

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,14 @@
+import classNames from "classnames/bind";
+import styles from "./Header.module.scss";
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h1>Header</h1>;
+    return (
+        <header className={cx("container")}>
+            <div className={cx("wrapper")}>Header</div>
+        </header>
+    );
 }
 
 export default Header;
